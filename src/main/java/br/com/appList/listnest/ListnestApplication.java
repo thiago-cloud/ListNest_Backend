74533590ff92a7b2108d25e79f1620ab4826cc73
thiago-cloud <thiago.154ti@gmail.com>
2024-11-12 22:19:55 -0300

    origin-ListNest

---
+++ src/main/java/br/com/appList/listnest/ListnestApplication.java
@@ -0,0 +1,13 @@
+package br.com.appList.listnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListnestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/appList/listnest/ListnestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appList.listnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
